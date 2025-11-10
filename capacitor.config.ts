963a015ceae053292faf93b9e20c5b1d7802cf67
NikitaKamennov <141999403+NikitaKamennov@users.noreply.github.com>
2025-11-10 09:49:52 +0400

    тот ещё коммит

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.tetris',
-  appName: 'Tetris',
+  appId: 'com.example.tanki',
+  appName: 'Tanki',
   webDir: 'dist',
  
   android: {
